c54ceb82497136cdb009eff78ff91b0df16eadee
Micheal Ibrahim <michealibrahim384@gmail.com>
2024-06-11 02:12:53 +0300

    MY_LIB.h

--- MY_LIB.h
+++ MY_LIB.h
@@ -24,86 +24,69 @@
 #define SYS_CLK_Internal_60MHz  RCC_PLLMul_15
 #define SYS_CLK_Internal_64MHz  RCC_PLLMul_16
 #define SYS_CLK_Internal_72MHz  RCC_PLLMul_18
-//w5500 BLOCK selection 
-#define COMMON_REG                  0b00000000
-#define Socket_n_Register(socket)  	0b00001000 | (socket<<5)
-#define Socket_n_TX_Buffer(socket)  0b00010000 | (socket<<5)
-#define Socket_n_RX_Buffer(socket)  0b00011000 | (socket<<5)
-
-// Offset Address for Common Register 
-#define W5500_MR 		0x0000	//mode register 
-#define W5500_GAR 		0x0001	//default gateway address 4 bytes
-#define W5500_SUBR		0x0005	//subnet mask 4 bytes
-#define W5500_SHAR		0x0009	//source MAC address 6 bytes
-#define W5500_SIPR		0x000F	//source IP address 4 bytes
-#define W5500_IR 		0x0015	//interrupt register
-#define W5500_IMR 		0x0016	//interrupt mask
-#define W5500_SIR 		0x0017	
-#define W5500_SIMR		0x0018
-#define W5500_PHYCFGR   0x002E
-#define W5500_CHIPV 	0x0039	//chip version register
-//useful for MR 
-#define RESET_BIT   0x80
-#define LINK_UP     0x01
-
-
-// Offset Address in Socket n Register Block
-#define W5500_SN_MR			0x0000	//socket mode register
-#define W5500_SN_CR			0x0001	//socket command register
-#define W5500_SN_IR			0x0002	//socket interrupt register 
-#define W5500_SN_SR			0x0003	//socket status register
-#define W5500_SN_PORT0		0x0004  //source port register 2 bytes
-#define W5500_SN_DHAR		0x0006  //destination MAC address 6 bytes
-#define W5500_SN_DIPR		0x000C	//destination IP address 4 byes
-#define W5500_SN_DPORT 		0x0010 	//destination PORT 2 bytes 
-#define W5500_SN_TX_FSR 	0x0020  //Socket n TX Free Size
-#define W5500_SN_TX_RD 		0x0022  //Socket n TX Read Pointer
-#define W5500_SN_TX_WR 		0x0024  //Socket n TX Write Pointer
-#define W5500_SN_RX_RSR		0x0026  //Socket n RX Received Size
-#define W5500_SN_RX_RD 		0x0028  //Socket n RX Read Pointer
-#define W5500_SN_RX_WR 		0x002A  //Socket n RX Write Pointer
-#define W5500_SN_Sn_FRAG0	0x002D	//Socket n Fragment Offset in IP header
-#define W5500_SN_Sn_FRAG1	0x002E
-
-//socket commands (writen to command register)
-#define W5500_CR_OPEN 		0x01
-#define W5500_CR_CONNECT 	0x04
-#define W5500_CR_DISCON		0x08
-#define W5500_CR_CLOSE 		0x10
-#define W5500_CR_SEND 		0x20
-#define W5500_CR_RECV 		0x40
-//useful for SR (status register)
-#define W5500_SOCK_INIT 		0x13
-#define W5500_SOCK_ESTABLISHED 	0x17
-#define W5500_SOCK_CLOSE_WAIT   0x1C
-
+#define SPI_flag_TXE 	2 
+#define SPI_flag_RXNE 	1
 
 void SET_SYS_CLK(uint32_t SYS_CLK_);
 
-//SPI functions 
+
+
+//-----GPIO
+void My_GPIO_init (GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin , uint8_t mode ,uint8_t speed);
+#define output_high(GPIOx,GPIO_Pin) GPIOx->BSHR = GPIO_Pin
+#define output_low(GPIOx,GPIO_Pin)  GPIOx->BCR = GPIO_Pin
+#define output_toggle(GPIOx,GPIO_Pin)  (GPIOx->OUTDR & GPIO_Pin) ? (output_low(GPIOx,GPIO_Pin)): (output_high(GPIOx,GPIO_Pin))
+#define input_read_Pin(GPIOx, GPIO_Pin)  GPIOx->INDR & GPIO_Pin
+#define input_read_Port(GPIOx) GPIOx->INDR 	
+#define output_read_Pin(GPIOx, GPIO_Pin)  GPIOx->OUTDR & GPIO_Pin 
+#define output_read_Port(GPIOx) 	GPIOx->OUTDR 
+#define output_write(GPIOx, GPIO_Pin , i)  (i) ? (outputhigh(GPIOx,GPIO_Pin)) : (output_low(GPIOx,GPIO_Pin)) 
+
+//-------SPI
+
 void My_SPI1_Master_Init(uint16_t NSS_PIN) ; //spi master clock polarity low clock phase 1st edge
-void My_SPI1_Send(uint8_t data); //sends data and doesn't check for receive (maybe can keep the data flow if used sequencially idk)
 void My_SPI1_Send_Frame(uint8_t* data , uint8_t len); //sends a series of data sequencially
 uint8_t My_SPI1_Transmit(uint8_t data ); //sends data byte and receives a byte 
 
-//w5500 module functions
-void W5500_write(uint16_t address,uint8_t block , uint8_t data);
-uint8_t W5500_read(uint16_t address,uint8_t block );
-uint8_t W5500_Connection_check(void);
-void W5500_setup(uint8_t *ip, uint8_t *mac); //sets up SPI1 with CS pin as A4 default gateway and subnet mask
-uint8_t W5500_connect(uint8_t socket, uint8_t *targetIP, uint16_t targetPort);
-void W5500_sendData(uint8_t socket, uint8_t *data, uint16_t len);
-uint16_t W5500_Size_Received(uint8_t socket);
-uint8_t W5500_receiveData(uint8_t socket);
 
-/*	example to enable an interrupt
-    NVIC_InitTypeDef NVIC_InitStructure;
-    NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
-    NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
-    NVIC_Init(&NVIC_InitStructure);
 
-    //NVIC_EnableIRQ(TIM2_IRQn);
-    SetVTFIRQ((u32)TIM2_IRQHandler,TIM2_IRQn,0,ENABLE);   <<------ important function
-*/
+//------RCC clock BS 
+#define GPIOA_EN() 					RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE)
+#define GPIOB_EN() 					RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE)
+#define GPIOC_EN() 					RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE)
+#define GPIOD_EN() 					RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOD, ENABLE)
+#define	GPIOE_EN()  				RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOE, ENABLE) 
+
+#define	AFIO_EN()   				RCC_APB2PeriphClockCmd(RCC_APB2Periph_AFIO, ENABLE)                
+#define	ADC1_EN()   				RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE)      
+#define	TIM1_EN()   				RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE)     
+#define	SPI1_EN()   				RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE)     
+#define	TIM8_EN()   				RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM8, ENABLE)   
+#define	USART1_EN() 				RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE)    
+#define	TIM9_EN()   				RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM9, ENABLE)    
+#define	TIM10_EN()  				RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM10, ENABLE)   
+																		
+#define	TIM2_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2    , ENABLE)   
+#define	TIM3_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3    , ENABLE)   
+#define	TIM4_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4    , ENABLE)   
+#define	TIM5_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM5    , ENABLE)   
+#define	TIM6_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM6    , ENABLE)   
+#define	TIM7_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM7    , ENABLE)   
+#define	UART6_EN()  				RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART6   , ENABLE)   
+#define	UART7_EN()  				RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART7   , ENABLE)   
+#define	UART8_EN()  				RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART8   , ENABLE)   
+#define	WWDG_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_WWDG    , ENABLE)   
+#define	SPI2_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI2    , ENABLE)   
+#define	SPI3_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_SPI3    , ENABLE)   
+#define	USART2_EN() 				RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART2  , ENABLE)   
+#define	USART3_EN() 				RCC_APB1PeriphClockCmd(RCC_APB1Periph_USART3  , ENABLE)   
+#define	UART4_EN()  				RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART4   , ENABLE)   
+#define	UART5_EN()  				RCC_APB1PeriphClockCmd(RCC_APB1Periph_UART5   , ENABLE)   
+#define	I2C1_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C1    , ENABLE)   
+#define	I2C2_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_I2C2    , ENABLE)   
+#define	USB_EN()    				RCC_APB1PeriphClockCmd(RCC_APB1Periph_USB     , ENABLE)   
+#define	CAN1_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN1    , ENABLE)   
+#define	CAN2_EN()   				RCC_APB1PeriphClockCmd(RCC_APB1Periph_CAN2    , ENABLE)   
+#define	BKP_EN()    				RCC_APB1PeriphClockCmd(RCC_APB1Periph_BKP     , ENABLE)   
+#define	PWR_EN()    				RCC_APB1PeriphClockCmd(RCC_APB1Periph_PWR     , ENABLE)   
+#define	DAC_EN()    				RCC_APB1PeriphClockCmd(RCC_APB1Periph_DAC     , ENABLE)   
